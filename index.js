ad5964333841d63311c9e00925cd807a9aa0f592
Saklain Ahmad <saklainhmd@gmail.com>
2022-12-19 23:29:32 +0530

    setup the view engine

--- index.js
+++ index.js
@@ -5,6 +5,12 @@
  //use express router
  app.use('/', require('./routes'));
 
+// setup the view engine
+ app.set('view engine', 'ejs');
+ app.set('views', './views');
+
+
+
  app.listen( port, function(err) {
     if(err) {
         console.log(`Error in running the Server ${err}`);
