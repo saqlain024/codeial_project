72ce29664dbc942f480e8a26b34ea97298636a40
Saklain Ahmad <saklainhmd@gmail.com>
2023-02-17 00:31:21 +0530

    added comments in the database

--- models/post.js
+++ models/post.js
@@ -8,7 +8,14 @@ const postSchema = new mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'      // refer to schema User
-    }
+    },
+     //include the array of ids of all comments in this postSchema itself
+     comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'comment'
+        }
+    ]
 },
     {
         timestamps: true            //it added two fields in robo3T and these are 'created at', 'updated at'
