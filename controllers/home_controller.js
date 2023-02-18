9cff991547033152ff398406f3040d8797f29860
Saklain Ahmad <saklainhmd@gmail.com>
2023-02-18 19:34:23 +0530

    converting the code in async await

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,29 +1,65 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
-module.exports.home = function(req, res) {
-    // console.log(req.cookies);          // printing cookies value on console
-    // res.cookie('user_id',25);        // we can change the modify cookie here too
-
+try{
+    module.exports.home =  async function(req, res) {
+        // console.log(req.cookies);          // printing cookies value on console
+        // res.cookie('user_id',25);        // we can change the modify cookie here too
     
-    // Post.find({}, function(err, posts){
-    //     return res.render('home', {
-    //         title : ' Codeial | Home',
-    //         posts : posts
-    //     });
-    // });
-
-    //above code 8-13 line is only fetching the user id 
-    // but if we want to fetch the each user object then below code  will work for that 18-24 line
-
-    // populate the user of each post
-    Post.find({}).populate('user').exec(function(err, posts){
+        // Post.find({}, function(err, posts){
+        //     return res.render('home', {
+        //         title : ' Codeial | Home',
+        //         posts : posts
+        //     });
+        // });
+    
+        //above code 8-13 line is only fetching the user id 
+        // but if we want to fetch the each user object then below code  will work for that 18-24 line
+    
+        // populate the user of each post
+        let posts  = await Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+    
+        });
+        // .exec(function(err, posts){
+    
+        //     User.find({}, function(err, users){
+        //         return res.render('home', {
+        //             title : ' Codeial | Home',
+        //             posts : posts,
+        //             all_users: users
+        //         });          
+        //     });
+        // });
+    
+        let users = await User.find({});
+    
         return res.render('home', {
             title : ' Codeial | Home',
-            posts : posts
+            posts : posts,
+            all_users: users
         });
-    });
+                        
+    }
 
-    
+}catch(err){
+    console.log('Error', err);
+    return;
 }
 
+
+
 //module.exports.actionName = function(req, res) { }
+
+//using then 
+// Post.find({}).populate('comments').then(function);
+
+// let posts = Post.find({}).populate('comments').exec();
+
+//posts.then();
+
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,11 +1,28 @@
 const User = require('../models/user');
 
-
-
+//lets keep at same as before
 module.exports.profile = function(req, res) {
-    return res.render('user_profile',{
-        title : 'User Profile'
-    })
+    User.findById(req.params.id, function(err, user) {
+        return res.render('user_profile',{
+            title : 'User Profile',
+            profile_user: user
+        })
+    });
+
+}
+
+
+module.exports.update = function(req, res) {
+    if(req.user.id == req.params.id) {
+
+        User.findByIdAndUpdate(req.params.id, req.body, function(err, user) {
+            return res.redirect('back');
+        });
+
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
+
 }
 
 
